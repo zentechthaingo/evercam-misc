24bab0c44529fe491946159ee36f794f3737c56a
Peter Wood <pwood@blacknorth.com>
2014-06-11 11:11:29 +0100

    Changes in support of enhanced error reporting.

--- lib/evercam_misc/errors.rb
+++ lib/evercam_misc/errors.rb
@@ -2,34 +2,62 @@
 
 module Evercam
   class EvercamError < StandardError
+    def initialize(message=nil, code=400, *context)
+      super(message)
+      @code    = code
+      @context = [].concat(context)
+    end
+
+    attr_reader :code, :context
+
+    def self.status_code
+      400
+    end
   end
 
   class AuthenticationError < EvercamError
-    def initialize(message=nil)
-      super(message || "Unauthenticated")
+    def initialize(message=nil, *context)
+      super((message || "Unauthenticated"), self.class.status_code, *context)
+    end
+
+    def self.status_code
+      401
     end
   end
 
   class AuthorizationError < EvercamError
-    def initialize(message=nil)
-      super(message || "Unauthorized")
+    def initialize(message=nil, *context)
+      super((message || "Unauthorized"), self.class.status_code, *context)
+    end
+
+    def self.status_code
+      403
     end
   end
 
   class NotFoundError < EvercamError
-    def initialize(message=nil)
-      super(message || "Not Found")
+    def initialize(message=nil, *context)
+      super((message || "Not Found"), self.class.status_code, *context)
+    end
+
+    def self.status_code
+      404
     end
   end
 
   class CameraOfflineError < EvercamError
-    def initialize(message=nil)
-      super(message || "Camera Offline")
+    def initialize(message=nil, *context)
+      super((message || "Camera Offline"), self.class.status_code, *context)
+    end
+
+    def self.status_code
+      501
     end
   end
 
   class OutcomeError < EvercamError
-    def initialize(outcome)
+    def initialize(outcome, message=nil, *context)
+      super(message, self.class.status_code, *context)
       @outcome = outcome
     end
 
@@ -37,24 +65,40 @@ module Evercam
       @outcome.errors.message_list.
         map(&:downcase)
     end
+
+    def self.status_code
+      400
+    end
   end
 
   module WebErrors
     class BadRequestError < EvercamError
-      def initialize(message=nil)
-        super(message || "Bad Request")
+      def initialize(message=nil, *context)
+        super((message || "Bad Request"), self.class.status_code, *context)
+      end
+
+      def self.status_code
+        400
       end
     end
 
     class ComingSoonError < EvercamError
-      def initialize(message=nil)
-        super(message || "Coming Soon")
+      def initialize(message=nil, *context)
+        super((message || "Coming Soon"), self.class.status_code, *context)
+      end
+
+      def self.status_code
+        501
       end
     end
 
     class ConflictError < EvercamError
-      def initialize(message=nil)
-        super(message || "Conflict")
+      def initialize(message=nil, *context)
+        super((message || "Conflict"), self.class.status_code, *context)
+      end
+
+      def self.status_code
+        409
       end
     end
   end
